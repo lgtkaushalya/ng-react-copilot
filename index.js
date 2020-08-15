7dfef926e0f0dea87ae6935cf6eaf3f5d7306743
lgtkaushalya <lgtkaushalya@gmail.com>
2020-08-15 19:36:04 +0530

    Improved the error type handling design and added basic reporting to the application

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const minimist = require('minimist');
 const files = require('./lib/files');
 const inquirer  = require('./lib/inquirer');
 const scan  = require('./lib/scan');
+const constants = require('./lib/constants');
+const reports = require('./lib/reports');
+const errors = require('./lib/errors');
 
 clear();
 
@@ -18,42 +21,64 @@ console.log(
 );
 
 const run = async () => {
-    let rootPath = process.argv[2] ? process.argv[2] : process.cwd();
-    inquirer.selectScanMode()
-        .then(answer => {
-            if (answer.mode == 'A single file') {
-                inquirer.askAFileToScan().then(fileAnswer => {
-                    scan.scan([fileAnswer.filePath]);
-                });
-            } else {
-                if (files.directoryExists(rootPath)) {
-                    const status = new Spinner('Scanning for files ...');
-                    status.start();
-                    const fileList = files.getListOfFilesToAnalyse(rootPath);
-                    scan.scan(fileList);
-                    status.stop();
-                } else {
-                    printErrorMessage('Root path does not exists');
-                }
+    try {
+        let rootPath = getRootPath();
+        inquirer.selectModes().then(answers => {
+            try {
+                let errorsToScan = getErrorsToScan(answers);
+                let scanResults = scanFiles(rootPath, answers, errorsToScan);
+                postProcessScanResults(scanResults, answers, errorsToScan);
+            } catch (e) {
+                printErrorMessage(e.message);
             }
-        })
-
-
-    /*
-
-
-    if (filelist.length) {
-        const filesToMigrate = inquirer.selectFilesToMigrate(filelist);
-    } else {
-        const filesToMigrate = await inquirer.askMigrationFiles();
+        });
+    } catch (e) {
+        printErrorMessage(e.message);
     }
-    console.log(chalk.blue.bgRed.bold(JSON.stringify(filesToMigrate)));*/
 };
 
-try {
-    run();
-} catch (e) {
-    printErrorMessage(e.message);
+
+run();
+
+function getErrorsToScan(answers) {
+    let allErrors = errors;
+    if (answers.errorMode == constants.errorMode_All) {
+        return allErrors;
+    } else {
+        return allErrors.filter(function(error) {
+            return answers.errorTypes.includes(error.value);
+        });
+    }
+}
+
+function scanFiles(rootPath, answers, errorsToScan) {
+    let status = new Spinner('Scanning for files ...');
+    status.start();
+    let scanResults = [];
+    if (answers.fileMode == constants.fileMode_Single && 'filePath' in answers) {
+        scanResults = scan.scanAFile(answers.filePath, errorsToScan);
+    } else {
+        scanResults = scan.scanADirectory(rootPath, errorsToScan);
+    }
+    status.stop();
+    return scanResults;
+}
+
+function postProcessScanResults(scanResults, answers, errorsToScan) {
+    reports.printHighlights(scanResults);
+
+    if (scanResults.ERRORS.length > 0 && answers.scanMode == constants.scanMode_Analyze) {
+        console.log(chalk.blue('The scan results are ready to be exported.'));
+        inquirer.selectReportType().then(answers => {
+            reports.printReport(scanResults, answers.reportType, errorsToScan);
+        });
+    } else if (answers.scanMode == constants.scanMode_Refactor) {
+
+    }
+}
+
+function getRootPath() {
+    return process.argv[2] ? process.argv[2] : process.cwd();
 }
 
 function printErrorMessage(message) {
